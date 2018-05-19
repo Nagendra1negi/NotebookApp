84d2106461c479a2ba8cf31a3590d6a6d6ab1c7d
rmmcosta <ricardocosta101085@gmail.com>
2018-05-19 17:57:03 +0100

    Finished the creation and save the message pages in a File

--- NotebookApp/FileModel.cs
+++ NotebookApp/FileModel.cs
@@ -9,21 +9,53 @@ namespace NotebookApp
 {
     class FileModel : IModel
     {
-        string _path = @"../Files/Pages.txt";
+        string _filePagesPath = @"../Files/Pages.txt";
+        string _fileIdPath = @"../Files/NextId.txt";
+        string _filesDir = @"../Files";
+        int _nextId = 1;
         StreamWriter _sw;
-        List<int>;
 
         public FileModel()
         {
-            if (!File.Exists(_path))
+            if (!Directory.Exists(_filesDir))
             {
-                _sw = new StreamWriter(File.Create(_path));
+                Directory.CreateDirectory(_filesDir);
+            }
+            if (!File.Exists(_filePagesPath))
+            {
+                File.Create(_filePagesPath);
+            }
+            if (!File.Exists(_fileIdPath))
+            {
+                using (StreamWriter sw = File.CreateText(_fileIdPath))
+                {
+                    sw.WriteLine(_nextId);
+                }
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(_fileIdPath);
+                _nextId = int.Parse(sr.ReadLine());
+                sr.Close();
             }
         }
 
         public void Create(IPageable page)
         {
-            _sw.WriteLine($"Id:{}");
+            _sw = new StreamWriter(_filePagesPath, append: true);
+            _sw.WriteLine("<Page>");
+            _sw.WriteLine($"<Id>{_nextId}</Id>");
+            _sw.WriteLine($"<Author>{page.Page.author}</Author>");
+            _sw.WriteLine($"<Title>{page.Page.title}</Title>");
+            if (page is Message pm)
+            {
+                _sw.WriteLine($"<Message>{pm.GetMessage}</Message>");
+            }
+            _sw.WriteLine($"</Page>");
+            _sw.Flush();
+            _sw.Close();
+            IncrementId();
+
         }
 
         public void Delete(int id)
@@ -49,6 +81,17 @@ namespace NotebookApp
         public List<IPageable> ReadAll()
         {
             throw new NotImplementedException();
+        }
+        
+        private void IncrementId()
+        {
+            _nextId += 1;
+            using (StreamWriter sw = new StreamWriter(_fileIdPath))
+            {
+                sw.WriteLine(_nextId);
+                sw.Flush();
+                sw.Close();
+            }
         }
     }
 }
